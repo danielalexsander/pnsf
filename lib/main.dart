10cbe52dd0d268d2eb83ff94d0870b6816e2c7dd
Daniel Alexsander <daniel.alexsander00@hotmail.com>
2024-08-26 11:12:57 -0300

    v6 App (online e offline)

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@
 * @category  [Cifras]
 * @author    Daniel Alexsander Inocêncio [daniel.alexsander00@hotmail.com]
 * @copyright [Daniel Alexsander 2024]
-* @version   v1
+* @version   v6
 * @since     02/07/2024
 */
 
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -15,6 +16,7 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 import 'package:pnsf/pages/cifra.dart';
 import 'package:pnsf/widgets/side_menu.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -52,39 +54,104 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List _foundCifra = [];
 
+  List<ConnectivityResult> _connectionStatus = [ConnectivityResult.none];
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<List<ConnectivityResult>> _connectivitySubscription;
+
+  static const snackBarConnection = SnackBar(
+    content: Text('Tem conexão, buscou da internet'),
+  );
+
+  static const snackBarnoConnection = SnackBar(
+    content: Text('Sem conexão, buscou do local'),
+  );
+
   @override
   void initState() {
+    initConnectivity();
+
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+
     readJson();
     _foundCifra = _cifras;
     super.initState();
   }
 
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initConnectivity() async {
+    late List<ConnectivityResult> resultConnection;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      resultConnection = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      print('Couldn\'t check connectivity status error: $e');
+      return;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(resultConnection);
+  }
+
+  Future<void> _updateConnectionStatus(
+      List<ConnectivityResult> resultConnection) async {
+    setState(() {
+      _connectionStatus = resultConnection;
+    });
+    // ignore: avoid_print
+    print('Connectivity changed: $_connectionStatus');
+  }
+
   // Função que lê o JSON
   Future<void> readJson() async {
-    /**
-    * VERSÃO ONLINE - BUSCA O JSON DO GITHUB RAW
-    */
-    var url = Uri.parse(
-        "https://raw.githubusercontent.com/danielalexsander/pnsf/master/assets/json/cifras.json");
-    Response response = await get(url);
+    Future.delayed(const Duration(seconds: 1), () async {
+      if (_connectionStatus.contains(ConnectivityResult.wifi) ||
+          _connectionStatus.contains(ConnectivityResult.mobile)) {
+        /**
+        * VERSÃO ONLINE - BUSCA O JSON DO GITHUB RAW
+        */
 
-    // // Caso precise do statuscode
-    // int statusCode = response.statusCode;
-    String json = response.body;
+        ScaffoldMessenger.of(context).showSnackBar(snackBarConnection);
+        var url = Uri.parse(
+            "https://raw.githubusercontent.com/danielalexsander/pnsf/master/assets/json/cifras.json");
+        Response response = await get(url);
 
-    final cifra = jsonDecode(json) as Map<String, dynamic>;
+        // // Caso precise do statuscode
+        // int statusCode = response.statusCode;
+        String json = response.body;
 
-    /**
-    * VERSÃO OFFLINE - BUSCA O JSON DO ASSETS
-    */
+        final cifra = jsonDecode(json) as Map<String, dynamic>;
 
-    // final String response =
-    //     await rootBundle.loadString('assets/json/cifras.json');
-    // final cifra = await json.decode(response) as Map<String, dynamic>;
+        setState(() {
+          _cifras = cifra["cifras"];
+          _foundCifra = cifra["cifras"];
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(snackBarnoConnection);
+        /**
+        * VERSÃO OFFLINE - BUSCA O JSON DO ASSETS
+        */
+        final String response =
+            await rootBundle.loadString('assets/json/cifras.json');
+        final cifra = await json.decode(response) as Map<String, dynamic>;
 
-    setState(() {
-      _cifras = cifra["cifras"];
-      _foundCifra = cifra["cifras"];
+        setState(() {
+          _cifras = cifra["cifras"];
+          _foundCifra = cifra["cifras"];
+        });
+      }
     });
   }
 
